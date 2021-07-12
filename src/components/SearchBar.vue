8d4f1f9fb7070e8cb3c313eb9f274175f2b05e0c
AlessioDPT <alessio.recos@gmail.com>
2021-07-12 16:35:53 +0200

    ADD- reset button

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -1,11 +1,11 @@
 <template>
 
     <div class="box text-end my-5">
-          <input type="text" placeholder="Inserisci parola chiave.." @keyup.enter="$emit('inputSearch',searchText)" v-model="searchText">
+          <input type="text" placeholder="Inserisci parola chiave.." @keyup.enter="$emit('inputSearch',searchText)" v-model.trim="searchText">
 
           <button type="submit" @click.prevent="$emit('inputSearch',searchText)">Ricerca</button>
 
-          <button type="reset">Annulla</button>
+          <button type="reset" @click.prevent="resetAll">Annulla</button>
       </div>
   
 </template>
@@ -16,6 +16,12 @@ export default {
     data(){
         return {
             searchText: ""
+        }
+    },
+    methods: {
+        resetAll(){
+            this.searchText = "";
+            this.$emit('inputSearch', this.searchText)
         }
     }
 
