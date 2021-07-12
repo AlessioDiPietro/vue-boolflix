5576c2d6f4dc9a2747cffa60a66c93ba047cf3c2
AlessioDPT <alessio.recos@gmail.com>
2021-07-12 16:25:50 +0200

    ADD- keyup in searchbar

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -1,7 +1,7 @@
 <template>
 
     <div class="box text-end my-5">
-          <input type="text" placeholder="Inserisci parola chiave.." v-model="searchText">
+          <input type="text" placeholder="Inserisci parola chiave.." @keyup.enter="$emit('inputSearch',searchText)" v-model="searchText">
 
           <button type="submit" @click.prevent="$emit('inputSearch',searchText)">Ricerca</button>
 
