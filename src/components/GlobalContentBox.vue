6f7c337ce56fb305b8a9b5b4810c96519bf71bd9
AlessioDPT <alessio.recos@gmail.com>
2021-07-13 17:20:29 +0200

    Fix- piccoli componenti

--- src/components/GlobalContentBox.vue
+++ src/components/GlobalContentBox.vue
@@ -28,6 +28,12 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+.box-film {
+    background-color: green;
+    }
+.box-series {
+    background-color: purple;
+    }
 
 </style>
