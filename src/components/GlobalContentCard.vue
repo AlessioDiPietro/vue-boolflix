02d0da3f3c433a199f047055df5991982ed34aa2
AlessioDPT <alessio.recos@gmail.com>
2021-07-13 20:16:00 +0200

    ADD- img sfondo poster (milestone3)

--- src/components/GlobalContentCard.vue
+++ src/components/GlobalContentCard.vue
@@ -1,13 +1,19 @@
 <template>
-  <div class="card">
-      <img src="" alt="">
-      <h3><strong>Titolo:</strong>{{info.title}}</h3>
-      <h5><strong>Titolo originale:</strong>{{info.original_title}}</h5>
-      <h5><strong>Lingua:</strong>{{info.original_language}}</h5>
-     
-      <h5><strong>Voto:</strong>{{info.vote_average}}</h5>
-      
-      
+  <div class="card-box  position-relative">
+      <img :src="info.poster_path == null ? 'https://cdn.browshot.com/static/images/not-found.png' : 'https://image.tmdb.org/t/p/w500/'+info.poster_path">
+      <div class="info_text position-absolute">
+
+        <ul>
+          <li><h3><strong>Titolo:</strong>{{info.title}}</h3></li>
+          <li><h5><strong>Titolo originale:</strong>{{info.original_title}}</h5></li>
+          <li>
+            <h6><strong>Lingua:</strong></h6>
+      <img :src="info.original_language == 'en' ? 'https://www.countryflags.io/gb/shiny/64.png':'https://www.countryflags.io/'+ info.original_language +'/shiny/64.png'">
+          </li>
+          <li><h5><strong>Voto:</strong>{{info.vote_average}}</h5></li>
+        </ul>
+
+      </div>  
   </div>
 </template>
 
@@ -20,18 +26,16 @@ export default {
       
       flagData: "en"
     }
-  },
-  // methods: {
-  //   changeFlag (){
-  //     if(this.info.original_language === "en") {
-  //       return this.flagData = "gb";
-  //     }
-  //   }
-  // }
-
+  }
 }
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+.info_text {
+  z-index: 999;
+  bottom:0
+  h6{
+      display: inline;
+  }
+}
 </style>
--- src/components/GlobalContentCardSeries.vue
+++ src/components/GlobalContentCardSeries.vue
@@ -1,12 +1,27 @@
 <template>
-  <div class="card">
+  <div class="card-box position-relative">
       
-      <img src="" alt="">
-      <h3><strong>Titolo:</strong>{{infoSeries.name}}</h3>
-      <h5><strong>Titolo originale:</strong>{{infoSeries.original_title}}</h5>
-      <h5><strong>Lingua:</strong>{{infoSeries.original_language}}</h5>
-      <img :src="'https://www.countryflags.io/'+ infoSeries.origin_country +'/shiny/64.png'">
-      <h5><strong>Voto:</strong>{{infoSeries.vote_average}}</h5>
+      <img :src="infoSeries.poster_path == null ? 'https://cdn.browshot.com/static/images/not-found.png' : 'https://image.tmdb.org/t/p/w500/'+infoSeries.poster_path">>
+      <div class="info_text position-absolute">
+          <ul>
+                <li><h3><strong>Titolo:</strong>{{infoSeries.name}}</h3></li>
+                <li><h5><strong>Titolo originale:</strong>{{infoSeries.original_title}}</h5></li>
+                <li>
+                  <h6><strong>Lingua:</strong></h6>
+                  <img :src="infoSeries.original_language == 'en' ? 'https://www.countryflags.io/bg/shiny/64.png':'https://www.countryflags.io/'+ infoSeries.original_language +'/shiny/64.png'">
+                </li>
+                <li>
+                  <h5><strong>Voto:</strong>{{infoSeries.vote_average}}</h5>
+                </li>
+              
+          </ul>
+      
+      
+      
+      
+      
+
+      </div>
       
       
   </div>
@@ -20,6 +35,16 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+.info_text {
+    z-index: 999;
+    bottom:0; 
+    
+    h6{
+        display: inline;
+    }
+}
+    
+
 
 </style>
