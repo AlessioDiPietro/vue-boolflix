25e6135794c8ab20a248d234bf749d5c9c5e9fff
AlessioDPT <alessio.recos@gmail.com>
2021-07-13 17:37:23 +0200

    ADD- flag change

--- src/components/GlobalContentCardSeries.vue
+++ src/components/GlobalContentCardSeries.vue
@@ -4,6 +4,7 @@
       <h3><strong>Titolo:</strong>{{infoSeries.name}}</h3>
       <h5><strong>Titolo originale:</strong>{{infoSeries.original_title}}</h5>
       <h5><strong>Lingua:</strong>{{infoSeries.original_language}}</h5>
+      <img :src="flag ==='en'? enFlag : itFlag" alt="">
       <h5><strong>Voto:</strong>{{infoSeries.vote_average}}</h5>
       
       
@@ -13,7 +14,21 @@
 <script>
 export default {
     name: "GlobalContentCardSeries",
-    props:["infoSeries"]
+    props:["infoSeries"],
+    data(){
+        return{
+            flag: "",
+            enFlag: "https://image.flaticon.com/icons/png/512/197/197374.png",
+            itFlag:"https://as1.ftcdn.net/jpg/01/77/89/96/500_F_177899610_lBWg6Wbvwla4ypmmk0PZNwcadHozvb3X.jpg"
+        }
+    },
+    created:{
+        changeFlag(){
+            if (this.infoSeries.original_language == "en"){
+                this.flag = this.enFlag;
+            }
+        }
+    }
 
 }
 </script>
