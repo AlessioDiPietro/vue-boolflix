dcf608fa376f0e8c7f8eb1c7fb408d63ed69b759
AlessioDPT <alessio.recos@gmail.com>
2021-07-13 18:11:50 +0200

    FIX- fonte bandiere

--- src/components/GlobalContentCardSeries.vue
+++ src/components/GlobalContentCardSeries.vue
@@ -1,10 +1,11 @@
 <template>
   <div class="card">
+      
       <img src="" alt="">
       <h3><strong>Titolo:</strong>{{infoSeries.name}}</h3>
       <h5><strong>Titolo originale:</strong>{{infoSeries.original_title}}</h5>
       <h5><strong>Lingua:</strong>{{infoSeries.original_language}}</h5>
-      <img :src="flag ==='en'? enFlag : frFlag" alt="">
+      <img :src="'https://www.countryflags.io/'+ infoSeries.origin_country +'/shiny/64.png'">
       <h5><strong>Voto:</strong>{{infoSeries.vote_average}}</h5>
       
       
@@ -14,21 +15,7 @@
 <script>
 export default {
     name: "GlobalContentCardSeries",
-    props:["infoSeries"],
-    data(){
-        return{
-            flag: "",
-            enFlag: "https://image.flaticon.com/icons/png/512/197/197374.png",
-            frFlag:"https://as1.ftcdn.net/jpg/01/77/89/96/500_F_177899610_lBWg6Wbvwla4ypmmk0PZNwcadHozvb3X.jpg"
-        }
-    },
-    created:{
-        changeFlag(){
-            if (this.infoSeries.original_language == "en"){
-                this.flag = this.enFlag;
-            }
-        }
-    }
+    props:["infoSeries"]
 
 }
 </script>
