a276e6de22d85879d1adb6b3d16015a9007b8cac
AlessioDPT <alessio.recos@gmail.com>
2021-07-15 16:51:31 +0200

    fix- styling

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -1,7 +1,7 @@
 <template>
 
     <div class="box m-2 text-end bg-transparent">
-          <input class="border-0 rounded-pill py-1 px-2 grey" type="text" placeholder="Ricerca tra le ultime uscite.." @keyup.enter="$emit('inputSearch',searchText)" v-model.trim="searchText">
+          <input class="border-0 rounded-pill py-1 px-2 grey" type="text" placeholder="Inserisci la/le parole chiave" @keyup.enter="$emit('inputSearch',searchText)" v-model.trim="searchText">
 
           <button class="btn btn-light mx-2" type="submit" @click.prevent="$emit('inputSearch',searchText)">Ricerca</button>
 
